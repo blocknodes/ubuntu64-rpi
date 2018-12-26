cfa7303cb28a59a47ac1197d563b3fa948c65944
blocknodes <41135212+blocknodes@users.noreply.github.com>
2018-12-26 17:36:51 +0800

    Update makeimage.sh

--- build/makeimage.sh
+++ build/makeimage.sh
@@ -2,16 +2,16 @@
 sudo apt-get install -y dosfstools dump parted kpartx -y
 
 sudo dd if=/dev/zero of=image.img bs=1M count=2100
-sudo parted image.img mktablel msdos
+sudo parted image.img mklablel msdos
 sudo parted image.img --script -- mkpart primary fat32 2048s 409600s
 sudo parted image.img --script -- mkpart primary ext4 409601s -1
 
 sudo kpartx image.img
 R= /dev/mapper/
-partBoot1= $(ls /dev/mapper | grep p1)
-partRoot1= $(ls /dev/mapper | grep p2)
-partBoot= $R$partBoot1
-partRoot= $R$partRoot1
+partBoot1=$(ls /dev/mapper | grep p1)
+partRoot1=$(ls /dev/mapper | grep p2)
+partBoot=$R$partBoot1
+partRoot=$R$partRoot1
 sudo mkfs.vfat $partBoot
 sudo mkfs.ext4 $partRoot
 sudo mount $partRoot /mnt
